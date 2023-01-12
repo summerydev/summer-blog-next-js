23ee4563df651bf2b38ff8c7deaada79b93d24f8
summer <summery.dev@gmail.com>
2023-01-12 17:52:02 +0900

    etc: footer content 추가
    - 깃허브 svg 추가

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <footer>
       <div>⋆ ₊ ﾟ ☽ * ₊ ⋆</div>
+      <div className="pb-2">₊</div>
+      <Link href={"https://github.com/summerydev"}>
+        <Image
+          src={`/social/github.svg`}
+          alt={`github`}
+          width={`30`}
+          height={`30`}
+        ></Image>
+      </Link>
     </footer>
   );
 }
