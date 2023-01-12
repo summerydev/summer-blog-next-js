ba477e052a3e44e9c5f2339d8baada1b2cd0bf2b
summer <summery.dev@gmail.com>
2023-01-12 17:16:23 +0900

    design: change PostList li color with dark mode

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -9,9 +9,13 @@ export default function PostLists({ posts }) {
           href={`/posts/${post._raw.flattenedPath}`}
           passHref
         >
-          <div className="p-3 mb-2 rounded-lg hover:scale-105 hover:bg-gray-200 hover:transition-all">
-            <div className={`font-medium text-xl`}>{post.title}</div>
-            <div className={`font-light`}>{post.description}</div>
+          <div
+            className={`p-3 mb-2 rounded-lg hover:scale-105 hover:bg-gray-200 hover:transition-all
+            hover:dark:bg-gray-600
+            `}
+          >
+            <div className={`font-medium text-2xl dark:hover:text-white`}>{post.title}</div>
+            <div className={`font-light pl-1 text-gray-600 dark:text-gray-300`}>{post.description}</div>
           </div>
         </Link>
       ))}
