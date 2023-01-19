7583d4e9e004fc9489a6470e71d713b8f6da2da5
summer <summery.dev@gmail.com>
2023-01-19 14:41:40 +0900

    feat: projects에 내용 추가

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,6 +1,8 @@
 import { Inter } from "@next/font/google";
 import Head from "next/head";
-import { metadata } from "../../data/data";
+import { metadata, projects, social } from "../../data/data";
+import Image from "next/image";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,11 +16,33 @@ export default function Projects() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <>
-          <div className={`text-center font-mono text-5xl font-extrabold`}>
-            🚧 개발중 🚜
-          </div>
-        </>
+        <div className={`text-center font-mono text-5xl font-extrabold`}>
+          🚧 개발중 🚜
+        </div>
+        <div className="flex flex-row flex-wrap grow justify-evenly">
+          {projects.map((project) => (
+            <div key={project.name} className={`basis-${projects.length}`}>
+              <Image
+                src={project.thumbNail}
+                alt={project.name}
+                width={`20`}
+                height={`20`}
+              ></Image>
+              <div>{project.name}</div>
+              <div className="flex flex-row space-x-1">
+                <Link href={project.repo}>
+                  <Image
+                    src={social.filter((el) => el.name == "github")[0].svg}
+                    alt={social.filter((el) => el.name == "github")[0].name}
+                    width={`20`}
+                    height={`20`}
+                  ></Image>
+                </Link>
+                <Link href={project.demo}>🔗</Link>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </>
   );
